a38c41c26920918e863514083eef1d7e807fea46
Willdorf <william.g.armstrong@gmail.com>
2015-11-08 18:04:23 -0500

    added bluetooth connectivity indicator

--- src/triplehorizontalbar1.c
+++ src/triplehorizontalbar1.c
@@ -9,6 +9,9 @@
 static Window *window;
 static Layer *s_layer;
 
+static BitmapLayer *s_bt_icon_layer;
+static GBitmap *s_bt_icon_bitmap;
+
 static uint8_t s_hour;
 static uint8_t s_min;
 static uint8_t s_sec;
@@ -30,15 +33,25 @@ GColor s_top_bar_color;
 GColor s_middle_bar_color;
 GColor s_bottom_bar_color;
 
+static void bluetooth_callback(bool connected) {
+	// show icon if disconnected
+	layer_set_hidden(bitmap_layer_get_layer(s_bt_icon_layer), connected);
+
+	if (!connected) {
+		//issue a vibrating alert
+		vibes_double_pulse();
+	}
+}
+
 static void update_time(struct tm * tick_time) {
 	s_hour = tick_time->tm_hour;
 	s_min = tick_time->tm_min;
 	s_sec = tick_time->tm_sec;
 	layer_mark_dirty(s_layer);
 
-//	if (twenty_four_hour_format == true) {
-//		APP_LOG(APP_LOG_LEVEL_DEBUG, "twenty_four_hour_format is true %s", twenty_four_hour_format ? "true" : "false");
-//	}
+	//	if (twenty_four_hour_format == true) {
+	//		APP_LOG(APP_LOG_LEVEL_DEBUG, "twenty_four_hour_format is true %s", twenty_four_hour_format ? "true" : "false");
+	//	}
 }
 
 static void set_background_and_text_color(int color) {
@@ -98,7 +111,7 @@ static void inbox_received_handler(DictionaryIterator *iter, void *context) {
 		if (background_color == 0) { //quick fix so that black colour persists
 			background_color++;
 		}
-		
+
 		persist_write_int(KEY_BACKGROUND_COLOR, background_color);
 
 		APP_LOG(APP_LOG_LEVEL_DEBUG, "background color: %d", background_color);
@@ -185,7 +198,7 @@ static void window_load(Window *window) {
 
 	if (persist_read_int(KEY_TOP_BAR_COLOR)) {
 		s_top_bar_color = GColorFromHEX(persist_read_int(KEY_TOP_BAR_COLOR));
-	///	set_background_and_text_color(background_color);
+		///	set_background_and_text_color(background_color);
 	} else {
 		s_top_bar_color = GColorRed;
 	}
@@ -206,10 +219,25 @@ static void window_load(Window *window) {
 		set_background_and_text_color(persist_read_int(KEY_BACKGROUND_COLOR));
 	} 
 
-//	if (persist_read_bool(KEY_TWENTY_FOUR_HOUR_FORMAT)) {
-//		twenty_four_hour_format = persist_read_bool(KEY_TWENTY_FOUR_HOUR_FORMAT);
-//	}
-	
+	//	if (persist_read_bool(KEY_TWENTY_FOUR_HOUR_FORMAT)) {
+	//		twenty_four_hour_format = persist_read_bool(KEY_TWENTY_FOUR_HOUR_FORMAT);
+	//	}
+
+	// Create the Bluetooth icon GBitmap
+	s_bt_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BT_ICON);
+
+	// Create the BitmapLayer to display the GBitmap
+	s_bt_icon_layer = bitmap_layer_create(GRect(0, 0, 30, 30));
+	bitmap_layer_set_bitmap(s_bt_icon_layer, s_bt_icon_bitmap);
+	layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_bt_icon_layer));
+
+	//show the correct state of the bluetooth connection from the start
+#ifdef PBL_SDK_2
+	bluetooth_callback(bluetooth_connection_service_peek());
+#elif PBL_SDK_3
+	bluetooth_callback(connection_service_peek_pebble_app_connection());
+#endif
+
 }
 
 static void window_unload(Window *window) {
@@ -222,6 +250,10 @@ static void window_unload(Window *window) {
 
 	//destroy the main layer
 	layer_destroy(s_layer);
+
+	//destroy the bluetooth stuffs
+	gbitmap_destroy(s_bt_icon_bitmap);
+	bitmap_layer_destroy(s_bt_icon_layer);
 }
 
 static void init(void) {
@@ -238,6 +270,15 @@ static void init(void) {
 
 	app_message_register_inbox_received(inbox_received_handler);
 	app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+
+	//Register for Bluetooth connections updates
+#ifdef PBL_SDK_2
+	bluetooth_connection_service_subscribe(bluetooth_callback);
+#elif PBL_SDK_3
+	connection_service_subscribe((ConnectionHandlers) {
+		.pebble_app_connection_handler = bluetooth_callback
+		});
+#endif
 
 	//display time right away
 	time_t start_time = time(NULL);
